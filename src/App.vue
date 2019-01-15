3e349ae7d806f4dd0afb8f44ef5726c38f602718
zyt <593607348@qq.com>
2019-01-15 10:04:14 +0800

    sure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app" class="content">
     <v-header></v-header>
-    <div class="tab">导航</div>
+    <div class="tab">
+      <div class="tab_item">商品</div>
+      <div class="tab_item">评价</div>
+      <div class="tab_item">商家</div>
+    </div>
     <div class="content"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import header from './components/header/header';
+import Vheader from './components/header/header';
 
 export default {
   name: '',
@@ -18,10 +22,14 @@ export default {
   },
   methods: {},
   components: {
-    'v-header': header
+    'v-header': Vheader
   }
 };
 </script>
-<style lang='stylus' rel='stylesheet/stylusnpm'>
-.content{text-align :center}
+<style lang='stylus' rel='stylesheet/stylus'>
+.app
+  .tab
+    display:fiex
+    width:33%
+
 </style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="header">
-        我是header
+        <div class="bg">
+            <div class="sell_box"> 我是header </div>
+            <div class="sell_box"> 头像</div>
+            <!-- <input class="sell_box" @click="this1"  disabled='false' value="点击事件" readonly='readonly' /> -->
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'header'
+  name: 'header',
+  methods: {
+    this1 () {
+      alert('dianjia');
+    }
+  }
 };
 </script>
 
