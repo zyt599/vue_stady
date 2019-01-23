59a965445537b7dda739414e15527e8e66307d3b
zyt <593607348@qq.com>
2019-01-23 17:15:29 +0800

    sure

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 <template>
 <div class="goods">
-    <div class="menu_wrapper">
+    <div class="menu_wrapper" ref="menuWrapper">
         <ul>
-            <li :key="item.text" v-for="item in goods" class="ment_item">
+            <li :key="item.text" v-for="(item,index) in goods" class="menu_item" :class="{'current':currentIndex===index}">
                 <span class="text border-1px" >
                     <span v-show="item.type>0" class="icon" :class="classmap[item.type]"></span>
                     {{item.name}}
@@ -10,50 +12,92 @@
             </li>
         </ul>
     </div>
-    <div class="foods_wrapper">
+    <div class="foods_wrapper" ref="foodsWrapper">
         <ul>
-            <li class="food_item border-1px" v-for="item in goods" :key="item.text">
-                <div class="title">{{item.name}}</div>
-                <div v-for="food in item.foods" :key="food.text" class="food">
-                    <span class="food_icon" ><img :src="food.icon"/></span>
-                    <span class="content">
-                        <div class="name">{{food.name}}</div>
-                        <p class="desc">{{food.description}}</p>
-                        <div>
-                            <span class="sell_count">月售：{{food.sellCount}}份</span>
-                            <span class="sell_count">好评率：{{food.rating}}%</span>
-                        </div>
-                        <div>
-                            <span>￥{{food.price}}</span>
-                            <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
-                            <i class="icon"></i>
-                        </div>
-                    </span>
-                </div>
+            <li class="food_item border-1px food_list_hook" v-for="item in goods" :key="item.text">
+                <ul>
+                    <span class="title">{{item.name}}</span>
+                    <li v-for="food in item.foods" :key="food.text" class="food">
+                        <span class="food_icon" ><img :src="food.icon" width="56" height="56"/></span>
+                        <span class="content">
+                            <div class="name">{{food.name}}</div>
+                            <p class="desc" v-show="food.description">{{food.description}}</p>
+                            <div class="sell">
+                                <span class="sell_count count">月售：{{food.sellCount}}份</span><span class="sell_count">好评率：{{food.rating}}%</span>
+                            </div>
+                            <div class="price">
+                                <span class="food_price">￥{{food.price}}</span>
+                                <span v-show="food.oldPrice" class="old_price">￥{{food.oldPrice}}</span>
+                                <i class="icon"></i>
+                            </div>
+                        </span>
+                    </li>
+                </ul>
             </li>
-
         </ul>
     </div>
 </div>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import BetterScroll from 'better-scroll';
 export default {
   props: {
     seller: {}
   },
   data () {
     return {
-      goods: []
+      goods: [],
+      listHeight: [],
+      scrollY: 0
     };
+  },
+  computed: {
+    currentIndex: function () {
+      for (var i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        console.log(i);
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
+        return 0;
+      }
+    }
   },
   created () {
     this.classmap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     this.$http.get('/api/goods').then((result) => {
       this.goods = result.body.data;
+      this.$nextTick(() => { // vue是异步更新，必须加$nextTick,才能在nextTick里面更新，实现滚动。
+        this._initScroll();
+        this._calculateHeight();
+      });
     }).catch((err) => {
       console.log(err);
     });
+  },
+  methods: {
+    //   初始化滚动条
+    _initScroll () {
+      this.menuScroll = new BetterScroll(this.$refs.menuWrapper, {});
+      this.foodScroll = new BetterScroll(this.$refs.foodsWrapper, {probeType: 3});
+      this.foodScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+    },
+    // 导航联动
+    _calculateHeight: function () {
+      let FoodList = this.$refs.foodsWrapper.getElementsByClassName('food_list_hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for (let i = 0; i < FoodList.length; i++) {
+        let item = FoodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    }
   }
 };
 </script>
@@ -72,12 +116,23 @@ export default {
         width 80px
         background #f3f5f7
         font-size 0
-        .ment_item
+        .menu_item
             display table
             height 54px
             width 100%
             line-height 14px
             padding 0 12px
+            background #7e8c8d
+            &.current
+                background #ffffff
+                font-weight 700
+                margin-top -1px
+                z-index 10
+                .text
+                  border-none()
+          &:last-child
+                .text
+                  border-none()
             .text
                 display table-cell
                 width 100%
@@ -85,7 +140,6 @@ export default {
                 font-size 12px
                 font-weight 200
                 border-1px(rgba(7,17,27,0.1))
-
             .icon
                 display inline-block
                 vertical-align top
@@ -94,7 +148,6 @@ export default {
                 margin-right 2px
                 background-size 12px 12px
                 background-repeat no-repeat
-                vertical-align text-top
                 &.decrease
                     bg_img('decrease_3')
                 &.discount
@@ -108,29 +161,61 @@ export default {
 
     .foods_wrapper
         flex 1
-        padding 0 14px
         .food_item
             display inline-block
             width 100%
             .food
                 display flex
                 border-1px(rgba(7,17,27,0.1))
-                margin-right 4px
+                margin 18px
+                padding-bottom 18px
+                &:last-child
+                    border-none()
+                    margin-bottom 0
                 .food_icon
-                    display inline-block
-                    flex 0
+                    flex 0 0 56px
+                    margin-right 10px
                 .content
-                    display inline-block
                     flex 1
                     .name
                         font-size 14px
                         line-height 14px
                         color rgb(7,17,27)
+                        margin 2px 0 8px 0
+                        height 14px
+                    .sell
+                        display inline
+                    .desc,.sell_count
+                        line-height 10px
+                        font-size 10px
+                        color rgb(147,153,159)
+                    .desc
+                        margin-bottom 8px
+                        line-height 12px
+                        .count
+                            margin-right 12px
+                    .price
+                        .food_price,.old_price
+                            font-weight 700
+                            line-height 24px
+                        .food_price
+                            font-size 14px
+                            color red
+                            margin-right 8px
+                        .old_price
+                            font-size 10px
+                            color rgb(147,153,159)
+                            text-decoration line-through
 
             .title
+                display block
                 font-size 12px
                 color rgb(147,153,159)
                 font-weight 700
-                line-height 27px
+                line-height 26px
+                border-left 2px solid #d9dde1
+                height 26px
+                background #f3f5f7
+                padding-left 14px
 
 </style>
